5ef5ba97ed1b46c5fea26978aa5a1b56b11bf405
PiPauwels <kruisje@gmail.com>
2018-06-01 10:48:57 +0200

    Removed import of waitForKeyElements.js-script, because disallowed by GreasyFork.

--- HLN-Kwaliteitsdebat.user.js
+++ HLN-Kwaliteitsdebat.user.js
@@ -2,7 +2,6 @@
 // @name     HLN Kwaliteitsdebat
 // @include  https://www.hln.be/*
 // @require  http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
-// @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @grant    GM_addStyle
 // ==/UserScript==
 
@@ -106,6 +105,55 @@ function Randoms(c, l, h)
     {
         return commentNumbers;
     };
+}
+
+function waitForKeyElements (selectorTxt, actionFunction, bWaitOnce, iframeSelector)
+{
+    var targetNodes, btargetsFound;
+
+    if (typeof iframeSelector == "undefined") targetNodes = $(selectorTxt);
+    else targetNodes = $(iframeSelector).contents().find (selectorTxt);
+    if (targetNodes && targetNodes.length > 0)
+    {
+        btargetsFound = true;
+        targetNodes.each ( function ()
+        {
+            var jThis = $(this);
+            var alreadyFound = jThis.data ('alreadyFound') || false;
+            if (!alreadyFound)
+            {
+                var cancelFound = actionFunction (jThis);
+                if (cancelFound) btargetsFound = false;
+                else jThis.data ('alreadyFound', true);
+            }
+        } );
+    }
+    else
+    {
+        btargetsFound = false;
+    }
+
+    var controlObj = waitForKeyElements.controlObj || {};
+    var controlKey = selectorTxt.replace (/[^\w]/g, "_");
+    var timeControl = controlObj [controlKey];
+
+    if (btargetsFound && bWaitOnce && timeControl)
+    {
+        clearInterval (timeControl);
+        delete controlObj [controlKey]
+    }
+    else
+    {
+        if (!timeControl)
+        {
+            timeControl = setInterval (function()
+            {
+                 waitForKeyElements (selectorTxt, actionFunction, bWaitOnce, iframeSelector);
+            }, 300);
+            controlObj [controlKey] = timeControl;
+        }
+    }
+    waitForKeyElements.controlObj = controlObj;
 }
 
 var commentIndex = 0;
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # HLN-Kwaliteitsdebat
+
+Tampermonkey script dat de Kwaliteit (met hoofdletter 'K') voorop plaatst in de commentaren op de website van de Belgische krant "Het Laatste Nieuws".
+
+## Script installeren
+
+Dit script maakt gebruik van User Script Manager Tampermonkey/Greasemonkey en is dus bruikbaar in Chrome, Microsoft Edge, Safari, Opera Next en Firefox. 
+
+### Vereisten
+
+Naast één van de hierboven vermelde browsers moet je eerst en vooral de plugin Tampermonkey installeren in je browser:
+* [Google Chrome Tampermonkey extension](https://chrome.google.com/webstore/detail/dhdgffkkebhmkfjojejmpbldmpobfkfo)
+* [Microsoft Edge Tampermonkey app](https://www.microsoft.com/store/apps/9NBLGGH5162S)
+* [Apple Safari extension](https://safari.tampermonkey.net/tampermonkey.safariextz)
+* [Mozilla Firefox addon](https://addons.mozilla.org/en-US/firefox/addon/tampermonkey/)
+* [Opera Next extension](https://addons.opera.com/en/extensions/details/tampermonkey-beta/)
+
+### Installatie
+
+
